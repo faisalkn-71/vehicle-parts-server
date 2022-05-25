d79545dac9aff4aabdd633a925a0c333b8974fcc
faisalkn-71 <csust2021@gmail.com>
2022-05-25 21:41:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Motor Parts is Running!')
+})
+
+app.listen(port, () => {
+  console.log(`Motor Parts listening on port ${port}`)
+})
