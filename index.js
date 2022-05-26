8b5ad56b8b9a4002fec9153c0347edb04901cd6f
faisalkn-71 <csust2021@gmail.com>
2022-05-26 18:53:19 +0600

    order-api-done

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqfuqlk.mongodb.net/?retryWrites=true&w=majority`;
-
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osz6xx4.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,6 +22,7 @@ async function run() {
 
     const productCollection = client.db('vehicleParts').collection('products');
     const reviewCollection = client.db('vehicleParts').collection('reviews');
+    const orderCollection = client.db('vehicleParts').collection('order');
 
 
     app.get('/products', async(req, res) => {
@@ -36,12 +33,39 @@ async function run() {
     })
 
 
-    app.get('/purchase/:id', async(req, res) =>  {
+    app.get('/products/:id', async(req, res) =>  {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const product = await productCollection.findOne(query);
       res.send(product)
     })
+
+
+
+    app.get('/order', async (req, res) => {
+      
+          const query = {};
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+      
+  })
+
+
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+      
+  })
+
+
+  app.delete('/order/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await orderCollection.deleteOne(query);
+    res.send(result);
+});
 
     
 
